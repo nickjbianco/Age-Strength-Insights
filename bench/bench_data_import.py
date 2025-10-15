54db8a76d376df0eca6ec40f4dc74ff3b20617b4
Nick Bianco <nickjbianco25@gmail.com>
2025-10-15 10:54:04 -0400

    Add files via upload
    Bench files

---
+++ bench/bench_data_import.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def import_data():
+    # Create engine
+    engine = create_engine("postgresql+psycopg2://postgres:Show1256x!@localhost:5433/powerlifting")
+
+    # Read the whole table into a DataFrame
+    df = pd.read_sql("SELECT name, age, sex, date, bodyweight_lbs, weightclass_lbs, bench_lbs FROM raw_bench", engine, parse_dates=["date"])
+
+    # Set the date as the index
+    df.set_index('date', inplace=True)
+
+    return df
